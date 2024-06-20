eaefe11837898a7c0d63627b1c70740e7b56b0cc
Kateřina Bedáňová <nesporovakaterina@gmail.com>
2024-06-20 20:44:28 +0200

    soubory pro záverecny projekt

---
+++ spolecne.js
@@ -0,0 +1,9 @@
+document.querySelector("#menu-tlacitko").addEventListener("click", () => {
+    document.querySelector("#menu-polozky").classList.toggle("show");
+  
+})
+
+document.querySelector("i").addEventListener("click", () => {
+    document.querySelector("i").classList.toggle("fa-xmark");
+  
+})
